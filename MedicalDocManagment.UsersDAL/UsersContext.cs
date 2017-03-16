5a62153445c9ef7d89f66b7e5289e23a17ccd5b5
Yaroslav Kostetskiy <ya.kosteckiy@gmail.com>
2017-03-16 13:01:42 +0200

    initial commit

---
+++ MedicalDocManagment.UsersDAL/UsersContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MedicalDocManagment.UsersDAL
+{
+    public class UsersContext : IdentityDbContext<User>
+    {
+        public UsersContext() : base("UsersDb") { }
+
+        public static UsersContext Create()
+        {
+            return new UsersContext();
+        }
+    }
+}
