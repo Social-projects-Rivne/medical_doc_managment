24500c9f9fea5797c7d5d32ef2880bd3993f955a
Andriy Katsubo <andriy_katsubo@ukr.net>
2017-03-18 21:48:19 +0200

    Created models for roles and users on front-end side.

---
+++ MedicalDocManagment.WebUI/app/models/rolemodel.ts
@@ -0,0 +1,9 @@
+﻿/**
+ * @fileoverview This file defines Role model, which represents role of the user.
+ * @author andriy_katsubo@ukr.net (Andriy Katsubo)
+ */
+
+export default class RoleModel {
+    id: number;
+    role: string;
+}
---
+++ MedicalDocManagment.WebUI/app/models/usermodel.ts
@@ -0,0 +1,16 @@
+﻿/**
+ * @fileoverview This file defines User model, which represents user of the system.
+ * @author andriy_katsubo@ukr.net (Andriy Katsubo)
+ */
+
+import RoleModel from './rolemodel';
+
+export default class UserModel{
+    id: number;
+    username: string;
+    email: string;
+    avatar: string;
+    position: string;
+    role: RoleModel;
+    isActive: boolean; 
+}
