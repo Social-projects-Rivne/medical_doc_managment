0acc305af77496c9345852fa3d1287c6b2fc0f15
Andriy Katsubo <andriy_katsubo@ukr.net>
2017-03-20 15:13:30 +0200

    Changed some filenames according to JS convention. Modified front-end side to show usersListComponent.

--- MedicalDocManagment.WebUI/app/app-module.ts
+++ MedicalDocManagment.WebUI/app/app-module.ts
@@ -1,10 +1,14 @@
 ﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
+
+import { AppComponent } from './app-component';
+import { UsersListComponent } from './userslist-component';
+import { UsersListItemComponent } from './userslistitem-component';
+
 @NgModule({
         imports: [BrowserModule, FormsModule],
-        declarations: [AppComponent],
+        declarations: [AppComponent, UsersListComponent, UsersListItemComponent],
         bootstrap: [AppComponent]
 })
 export class AppModule { }
