69d1621f71a721c244c1aa9e978cdf5829b944bd
Andriy Katsubo <andriy_katsubo@ukr.net>
2017-03-20 17:31:13 +0200

    Added display of user's avatar in users list.

--- MedicalDocManagment.WebUI/app/userslist-component.ts
+++ MedicalDocManagment.WebUI/app/userslist-component.ts
@@ -21,9 +21,9 @@ export class UsersListComponent {
     getUsersList(): void {
         // mock-up
         this.users = new Array<UserModel>(
-            { id: 1, username: 'Іван', email: 'ivan@lala.net', avatar: '1', position: 'лікар', role: { id: 1, role: 'роль 1' }, isActive: true },
-            { id: 2, username: 'Степан', email: 'stepan@lala.net', avatar: '1', position: 'лікар', role: { id: 1, role: 'роль 1' }, isActive: true },
-            { id: 3, username: 'Сергій', email: 'sergiy@lala.net', avatar: '1', position: 'лікар', role: { id: 2, role: 'роль 2' }, isActive: false }
+            { id: 1, username: 'Іван', email: 'ivan@lala.net', avatar: '\\avatars\\fire_01.gif', position: 'лікар', role: { id: 1, role: 'роль 1' }, isActive: true },
+            { id: 2, username: 'Степан', email: 'stepan@lala.net', avatar: '\\avatars\\fire_01.gif', position: 'лікар', role: { id: 1, role: 'роль 1' }, isActive: true },
+            { id: 3, username: 'Сергій', email: 'sergiy@lala.net', avatar: '\\avatars\\girl_with_cigarette.jpg', position: 'лікар', role: { id: 2, role: 'роль 2' }, isActive: false }
         );
     }
 
