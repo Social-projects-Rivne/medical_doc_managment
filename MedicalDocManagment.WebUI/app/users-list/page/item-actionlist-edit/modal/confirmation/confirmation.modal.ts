b961f7d59183beda6cd1f543dceb73828135fc72
Andriy Katsubo <andriy_katsubo@ukr.net>
2017-04-02 16:28:51 +0300

    Implemented feature "confirmation window for edit user".

---
+++ MedicalDocManagment.WebUI/app/users-list/page/item-actionlist-edit/modal/confirmation/confirmation.modal.ts
@@ -0,0 +1,25 @@
+﻿import { Component, EventEmitter, Output } from '@angular/core';
+
+declare var jQuery: any;
+
+@Component({
+    moduleId: module.id,
+    selector: '[edit-confirmation]',
+    templateUrl: 'confirmation.modal.html'
+})
+export default class ConfirmationModal {
+    @Output() onConfirmedEdit: EventEmitter<void>;
+
+    constructor() {
+        this.onConfirmedEdit = new EventEmitter<void>();
+    }
+
+    hideModal(): void {
+        jQuery("#userEditConfirmationModal").modal("hide");
+    }
+
+    onYes(): void {
+        this.hideModal();
+        this.onConfirmedEdit.emit();
+    }
+}
