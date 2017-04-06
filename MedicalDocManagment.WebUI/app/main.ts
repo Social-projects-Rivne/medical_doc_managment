e95ba4a2bd7378d9b1585659eb4d33949bfff578
Andriy Katsubo <andriy_katsubo@ukr.net>
2017-04-06 15:02:35 +0300

    Split index.html into three separate index.html, each for UI application.

--- MedicalDocManagment.WebUI/app/main.ts
+++
@@ -1,9 +0,0 @@
-﻿import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { LoginAppModule } from './core/login-app.module';
-import { AdminAppModule } from './admin/admin-app.module';
-import { MainAppModule } from './main/main-app.module';
-
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(LoginAppModule);
-platform.bootstrapModule(AdminAppModule);
-platform.bootstrapModule(MainAppModule);
