f3c7758d29d2ddb28d9c91a724977b556f8f4027
Yaroslav Kostecki <Yaroslav Kostecki>
2017-04-11 00:01:53 +0300

    added repositories for BlockMkh and ClassMkh models

---
+++ MedicalDocManagment.DAL/Repository/ClassMkhRepository.cs
@@ -0,0 +1,12 @@
+﻿using MedicalDocManagment.DAL.Enities;
+using MedicalDocManagment.DAL.Repository.Interfaces;
+
+namespace MedicalDocManagment.DAL.Repository
+{
+    public class ClassMkhRepository : GenericRepository<ClassMkh>, IClassMkhRepository
+    {
+        public ClassMkhRepository(Context context) : base(context)
+        {
+        }
+    }
+}
