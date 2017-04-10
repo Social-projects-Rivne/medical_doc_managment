c8fc3c4399c6e708b55deba2ec9ea0736fefee4d
Yaroslav Kostecki <Yaroslav Kostecki>
2017-04-11 00:03:02 +0300

    added BlockMkh and ClassMkh models

---
+++ MedicalDocManagment.DAL/Enities/BlockMkh.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedicalDocManagment.DAL.Enities
+{
+    public class BlockMkh
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string Id { get; set; }
+        [StringLength(200)]
+        public string Name { get; set; }
+        public string ClassId { get; set; }
+        [ForeignKey("ClassId")]
+        public virtual ClassMkh ClassMkh { get; set; }
+    }
+}
---
+++ MedicalDocManagment.DAL/Enities/ClassMkh.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedicalDocManagment.DAL.Enities
+{
+    public class ClassMkh
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string Id { get; set; }
+        [StringLength(200)]
+        public string Name { get; set; }
+        public virtual ICollection<BlockMkh> BlocksMkh { get; set; }
+    }
+}
