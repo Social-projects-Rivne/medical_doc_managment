e81e7338c51a947e42d878b524392ae5659cd317
Yaroslav Kostecki <Yaroslav Kostecki>
2017-04-10 23:57:22 +0300

    removed useless projects

--- MedicalDocManagment.UsersDAL/Repositories/Interfaces/IPositionRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using MedicalDocManagment.UsersDAL.Entities;
-
-namespace MedicalDocManagment.UsersDAL.Repositories.Interfaces
-{
-    public interface IPositionRepository : IGenericRepository<Position>
-    {
-    }
-}
