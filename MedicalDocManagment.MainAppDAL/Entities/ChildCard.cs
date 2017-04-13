fac907d78978c69a58edcadc4cb78310b0aa9592
Andriy <andriy_katsubo@ukr.net>
2017-04-13 17:52:45 +0300

    Pull request for feature/method for adding new patient into the system (#50)
    * Implemented feature — method for adding new patient into the system.

---
+++ MedicalDocManagment.MainAppDAL/Entities/ChildCard.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedicalDocManagment.MainAppDAL.Entities
+{
+    [Table("ChildrenCards")]
+    public class ChildCard
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        [StringLength(100)]
+        public string SecondName { get; set; }
+
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime CheckIn { get; set; }
+
+        public DateTime? CheckOut { get; set; }
+
+        public string DiagnosisId { get; set; }
+
+        [ForeignKey("DiagnosisId")]
+        public virtual Diagnosis Diagnosis { get; set; }
+
+        public string Prescription { get; set; }
+
+        public string DirectedBy { get; set; }
+
+        public virtual ICollection<ParentChildCard> ParentsChildren { get; set; }
+    }
+}
---
+++ MedicalDocManagment.MainAppDAL/Entities/Parent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedicalDocManagment.MainAppDAL.Entities
+{
+    public class Parent
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        [StringLength(100)]
+        public string SecondName { get; set; }
+
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        public string Work { get; set; }
+
+        public string Phone { get; set; }
+
+        public virtual ICollection<ParentChildCard> ParentsChildren { get; set; }
+    }
+}
