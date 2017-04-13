148e5e8e51facff171a596dedb372d9966980e60
Yaroslav Kostetskiy <ya.kosteckiy@gmail.com>
2017-04-13 11:28:05 +0300

    Added DiagnosesMkh's DbSet

--- MedicalDocManagment.DAL/Context.cs
+++ MedicalDocManagment.DAL/Context.cs
@@ -11,6 +11,7 @@ namespace MedicalDocManagment.DAL
         public DbSet<ClassMkh> ClassesMkh { get; set; }
         public DbSet<BlockMkh> BlocksMkh { get; set; }
         public DbSet<NosologyMkh> NosologiesMkh { get; set; }
+        public DbSet<DiagnosisMkh> DiagnosesMkh { get; set; }
 
         static Context()
         {
