39ae9f729d8abd365d504bb5abb35c7c2bc821cd
Andriy <andriy_katsubo@ukr.net>
2017-04-15 18:11:29 +0300

    Changed feature component type from modal to form.
    Renamed some properties according to style guide. (#53)

--- MedicalDocManagment.WebUI/app/main/models/parent.model.ts
+++ MedicalDocManagment.WebUI/app/main/models/parent.model.ts
@@ -1,25 +1,25 @@
 ﻿export default class ParentModel {
     id: string;
-    f_name: string;
-    s_name: string;
-    l_name: string;
+    firstName: string;
+    secondName: string;
+    lastName: string;
     work: string;
     phone: string;
 
     constructor(jsonObject?) {
         if (jsonObject) {
             this.id = jsonObject.id;
-            this.f_name = jsonObject.f_name;
-            this.s_name = jsonObject.s_name;
-            this.l_name = jsonObject.l_name;
+            this.firstName = jsonObject.firstName;
+            this.secondName = jsonObject.secondName;
+            this.lastName = jsonObject.lastName;
             this.work = jsonObject.work;
             this.phone = jsonObject.phone;
         }
         else {
             this.id = null;
-            this.f_name = null;
-            this.s_name = null;
-            this.l_name = null;
+            this.firstName = null;
+            this.secondName = null;
+            this.lastName = null;
             this.work = null;
             this.phone = null;
         }
