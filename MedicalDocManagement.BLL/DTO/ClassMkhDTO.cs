605eb54e5da906b5eed2095b2c9b438ad3d65b25
Artem Panko <artempankozoria7@gmail.com>
2017-04-15 22:49:08 +0300

    Added api for getting diagnoses and added child cards service

---
+++ MedicalDocManagement.BLL/DTO/ClassMkhDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalDocManagement.BLL.DTO
+{
+    public class ClassMkhDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ MedicalDocManagment.WebUI/Models/Main/DiagnosisMkhVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MedicalDocManagment.WebUI.Models.Main
+{
+    public class DiagnosisMkhVM
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
