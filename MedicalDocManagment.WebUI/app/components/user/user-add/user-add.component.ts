7d0028bbb351d9c12cd6ae1e13e7e4523882330b
Yaroslav Kostetskiy <ya.kosteckiy@gmail.com>
2017-04-23 23:24:30 +0300

    Hotfix/single ui application (#65)
    * made reorganization for all UI application
    
    * added  folder into gitignore file
    
    * added  folder into
    
    * created single UI app that contain 3 other
    
    * Update .gitignore
    
    * Fixed routing in single ui application. Added header into main app component.
    
    * added DI for authService
    
    * Removed useless folder and file import.

---
+++ MedicalDocManagment.WebUI/app/components/user/user-add/user-add.component.ts
@@ -0,0 +1,50 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { UserService} from '../../../services/user.service';
+import {User} from '../../../models/user';
+import { NotificationsService, SimpleNotificationsComponent } from 'angular2-notifications';
+import PositionModel from "../../../models/positionmodel";
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-user-add',
+    templateUrl: 'user-add.component.html',
+    providers: [UserService, NotificationsService]
+})
+export class UserAddComponent implements OnInit {
+    user: User = new User();
+    positions: PositionModel[];
+    public notificationOptions = {
+        timeOut: 5000,
+        lastOnBottom: true,
+        clickToClose: true,
+        maxLength: 0,
+        maxStack: 7,
+        showProgressBar: true,
+        pauseOnHover: false,
+        preventDuplicates: false,
+        preventLastDuplicates: 'visible',
+        animate: 'scale',
+        position: ['right', 'bottom']
+    };
+    constructor(private userService: UserService, private _service: NotificationsService) {}
+    ngOnInit() {
+        this.updatePositionsList();
+    }
+    submit() {
+        this.userService.postData(this.user)
+                        .subscribe(
+                        (data) => {
+                            console.log(data);
+                            this._service.success("Успіх", "Успішно додано користувача");
+                        },
+                        (error) => {
+                            console.log(error);
+                            this._service.error("Помилка", "Відбулась помилка при додаванні користувача");
+                        }
+        );
+    }
+    updatePositionsList(): void {
+        this.userService.getPositionsList()
+            .subscribe((data: PositionModel[]) => { this.positions = data;});
+    }
+}
---
+++ MedicalDocManagment.WebUI/app/services/children-card.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { Inject } from '@angular/core'; 
+import { Http, Response, Headers, URLSearchParams } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+import ChildCardModel from '../models/child-card.model';
+import ChildrenCardsModel from '../models/children-cards.model';
+import ParentModel from '../models/parent.model';
+import { AuthenticationService } from "./authentication.service";
+
+@Injectable()
+export default class ChildrensCardService {
+    private _apiUrl: string = '/api/childcards';
+    private _headers: Headers;
+
+    constructor(private _http: Http, @Inject(AuthenticationService) private _authenticationService: AuthenticationService) {
+        this._headers = new Headers({ 'Content-Type': 'application/json;charset=utf-8' });
+        this._headers.append('Authorization', 'Bearer ' + this._authenticationService.token);
+    }
+    
+    addChildrenCard(childrensCard: ChildCardModel): boolean {
+        if (childrensCard) {
+            console.log(childrensCard);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Method sends to server request for adding new parent.
+     * @param {ParentModel} parent Contains data about parent to add
+     * @return {Observable<ParentModel>} Model, which contains added data of parent.
+     */
+    addParent(parent: ParentModel): Observable<ParentModel> {
+        let body:string = JSON.stringify(parent);
+        let headers = this._headers;
+        return this._http.post('/api/childcards/addparent', body, { headers: headers })
+            .map((resp: Response) => {
+                Observable.of(new ParentModel(resp.json()));
+            })
+            .catch((error: any) => { return Observable.throw(error); });
+    }
+
+    getChildrenCards(): Observable<ChildrenCardsModel> {
+        let headers = this._headers;
+        return this._http.get('/api/childcards/GetChildrenCards', { headers })
+           .map((resp: Response) => { return new ChildrenCardsModel(resp.json()); })
+           .catch((error: any) => { return Observable.throw(error); });
+    }
+
+}
