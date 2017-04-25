f6f1387d2d1fd1b2652c5399a1194bb5c042fb27
Yaroslav Kostetskiy <ya.kosteckiy@gmail.com>
2017-04-25 17:16:36 +0300

    changed addChildrenCard function

--- MedicalDocManagment.WebUI/app/services/children-card.service.ts
+++ MedicalDocManagment.WebUI/app/services/children-card.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@angular/core';
-import { Inject } from '@angular/core'; 
-import { Http, Response, Headers, URLSearchParams } from '@angular/http';
+﻿import { Injectable } from '@angular/core';
+import { Inject } from '@angular/core';
+import { Http } from '@angular/http';
+import { Response, Headers, URLSearchParams } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
@@ -13,22 +15,19 @@ import { AuthenticationService } from "./authentication.service";
 
 @Injectable()
 export default class ChildrensCardService {
-    private _apiUrl: string = '/api/childcards';
-    private _headers: Headers;
+    private headers: Headers;
 
-    constructor(private _http: Http, @Inject(AuthenticationService) private _authenticationService: AuthenticationService) {
-        this._headers = new Headers({ 'Content-Type': 'application/json;charset=utf-8' });
-        this._headers.append('Authorization', 'Bearer ' + this._authenticationService.token);
+    constructor(private http: Http, @Inject(AuthenticationService) private _authenticationService: AuthenticationService) {
+        this.headers = new Headers({ 'Content-Type': 'application/json;charset=utf-8' });
+        this.headers.append('Authorization', 'Bearer ' + this._authenticationService.token);
     }
-    
-    addChildrenCard(childrensCard: ChildCardModel): boolean {
-        if (childrensCard) {
-            console.log(childrensCard);
 
-            return true;
-        }
-
-        return false;
+    addChildrenCard(obj: ChildCardModel) {
+        const body = JSON.stringify(obj);
+        let headers = this.headers;
+        return this.http.post('/api/childcards/addpatient', body, { headers: headers })
+            .map((resp: Response) => resp.json())
+            .catch((error: any) => { return Observable.throw(error); });
     }
 
     /**
@@ -38,8 +37,8 @@ export default class ChildrensCardService {
      */
     addParent(parent: ParentModel): Observable<ParentModel> {
         let body:string = JSON.stringify(parent);
-        let headers = this._headers;
-        return this._http.post('/api/childcards/addparent', body, { headers: headers })
+        let headers = this.headers;
+        return this.http.post('/api/childcards/addparent', body, { headers: headers })
             .map((resp: Response) => {
                 Observable.of(new ParentModel(resp.json()));
             })
@@ -47,8 +46,8 @@ export default class ChildrensCardService {
     }
 
     getChildrenCards(): Observable<ChildrenCardsModel> {
-        let headers = this._headers;
-        return this._http.get('/api/childcards/GetChildrenCards', { headers })
+        let headers = this.headers;
+        return this.http.get('/api/childcards/GetChildrenCards', { headers })
            .map((resp: Response) => { return new ChildrenCardsModel(resp.json()); })
            .catch((error: any) => { return Observable.throw(error); });
     }
