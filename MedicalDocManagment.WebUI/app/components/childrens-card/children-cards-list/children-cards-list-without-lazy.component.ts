29b052bad8443f4b3142c0b5574bfa1cafd7d28e
Andriy <andriy_katsubo@ukr.net>
2017-04-26 23:39:27 +0300

    Fixed broken feature. Optimized code. (#79)

---
+++ MedicalDocManagment.WebUI/app/components/childrens-card/children-cards-list/children-cards-list-without-lazy.component.ts
@@ -0,0 +1,22 @@
+﻿import { Component, Input } from '@angular/core';
+import { Inject } from '@angular/core'; 
+import { Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Rx';
+
+import ChildrensCardService from '../../../services/children-card.service';
+import ChildrenCardsModel from '../../../models/children-cards.model';
+
+@Component({
+    moduleId: module.id,
+    selector: 'children-cards-list-without-lazy',
+    templateUrl: 'children-cards-list-without-lazy.component.html'
+})
+export default class ChildrenCardsListWithoutLazyComponent {
+    @Input() childrenCards: ChildrenCardsModel;
+
+    constructor() {
+        this.childrenCards = [];
+    }
+}
+
