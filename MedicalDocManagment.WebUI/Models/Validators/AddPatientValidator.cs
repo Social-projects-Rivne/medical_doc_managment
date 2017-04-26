9e2df960d0f5d126600b5b1486f3f7bdf97cb86c
Yaroslav Kostetskiy <ya.kosteckiy@gmail.com>
2017-04-26 23:30:59 +0300

    moved validation expression from RuleSet

--- MedicalDocManagment.WebUI/Models/Validators/AddPatientValidator.cs
+++ MedicalDocManagment.WebUI/Models/Validators/AddPatientValidator.cs
@@ -7,47 +7,41 @@ namespace MedicalDocManagment.WebUI.Models.Validators
     {
         public AddPatientVMValidator()
         {
-            RuleSet("Names", () =>
-            {
-                RuleFor(patient => patient.FirstName)
-                       .Matches(@"[А-Яа-яґҐєЄіІїЇ`´ʼ’ʼ’'-]{1,100}")
-                       .WithMessage("Ім'я пацієнта написане не кирилицею");
+            RuleFor(patient => patient.FirstName)
+                   .Matches(@"[А-Яа-яґҐєЄіІїЇ`´ʼ’ʼ’'-]{1,100}")
+                   .WithMessage("Ім'я пацієнта написане не кирилицею");
 
-                RuleFor(patient => patient.SecondName)
-                       .Matches(@"[А-Яа-яґҐєЄіІїЇ`´ʼ’ʼ’'-]{1,100}")
-                       .WithMessage("По батькові пацієнта написане не кирилицею");
+            RuleFor(patient => patient.SecondName)
+                   .Matches(@"[А-Яа-яґҐєЄіІїЇ`´ʼ’ʼ’'-]{1,100}")
+                   .WithMessage("По батькові пацієнта написане не кирилицею");
 
-                RuleFor(patient => patient.LastName)
-                       .Matches(@"[А-Яа-яґҐєЄіІїЇ`´ʼ’ʼ’'-]{1,100}")
-                       .WithMessage("Прізвище пацієнта написане не кирилицею");
+            RuleFor(patient => patient.LastName)
+                   .Matches(@"[А-Яа-яґҐєЄіІїЇ`´ʼ’ʼ’'-]{1,100}")
+                   .WithMessage("Прізвище пацієнта написане не кирилицею");
 
-                RuleFor(patient => patient.LastName)
-                       .NotNull()
-                       .WithMessage("Для додавання пацієнта необхідне його прізвище");
+            RuleFor(patient => patient.LastName)
+                   .NotNull()
+                   .WithMessage("Для додавання пацієнта необхідне його прізвище");
 
-                RuleFor(patient => patient.SecondName)
-                       .NotNull()
-                       .WithMessage("Для додавання пацієнта необхідне його по батькові");
+            RuleFor(patient => patient.SecondName)
+                   .NotNull()
+                   .WithMessage("Для додавання пацієнта необхідне його по батькові");
 
-                RuleFor(patient => patient.FirstName)
-                       .NotNull()
-                       .WithMessage("Для додавання пацієнта необхідне його ім'я");
-            });
+            RuleFor(patient => patient.FirstName)
+                   .NotNull()
+                   .WithMessage("Для додавання пацієнта необхідне його ім'я");
 
-            RuleSet("Dates", () =>
-            {
-                RuleFor(patient => patient.Checkin)
-                       .NotNull()
-                       .WithMessage("Початкова дата обліку пацієнта є обов'язковою");
+            RuleFor(patient => patient.Checkin)
+                   .NotNull()
+                   .WithMessage("Початкова дата обліку пацієнта є обов'язковою");
 
-                RuleFor(patient => patient.Checkin)
-                       .Must(IsValidCheckIn)
-                       .WithMessage("Початкова дата обліку пацієнта є більшою за теперішню дату");
+            RuleFor(patient => patient.Checkin)
+                   .Must(IsValidCheckIn)
+                   .WithMessage("Початкова дата обліку пацієнта є більшою за теперішню дату");
 
-                RuleFor(patient => patient.Date)
-                       .Must(IsValidDate)
-                       .WithMessage("Дата народження пацієнта є більшою за сьогоднішню");
-            });
+            RuleFor(patient => patient.Date)
+                   .Must(IsValidDate)
+                   .WithMessage("Дата народження пацієнта є більшою за сьогоднішню");
 
             RuleFor(patient => patient.Address)
                    .Matches(@"[А-Яа-яґҐєЄіІїЇ`´ʼ’ʼ’'-]{1,100}")
@@ -66,16 +60,6 @@ namespace MedicalDocManagment.WebUI.Models.Validators
         private bool IsValidCheckIn(DateTime checkIn)
         {
             return DateTime.Compare(checkIn, DateTime.Now) <= 0;
-        }
-
-        private bool IsValidCheckOut(AddPatientVM addPatientVM)
-        {
-            if (!addPatientVM.Checkout.HasValue)
-            {
-                return true;
-            }
-
-            return DateTime.Compare(addPatientVM.Checkin, (DateTime)addPatientVM.Checkout) <= 0;
         }
     }
 }
