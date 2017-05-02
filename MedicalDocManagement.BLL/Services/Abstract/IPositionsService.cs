a76ebb3eb4b670042cbe987602f9451eb991d466
Ravennod <ravennod@gmail.com>
2017-05-02 17:37:38 +0300

    Fixed bug automapping users list and positions list.

---
+++ MedicalDocManagement.BLL/Services/Abstract/IPositionsService.cs
@@ -0,0 +1,15 @@
+﻿using MedicalDocManagment.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalDocManagement.BLL.Services.Abstract
+{
+    public interface IPositionsService
+    {
+        List<PositionDTO> GetPositions();
+        PositionDTO GetPosition(int id);
+    }
+}
---
+++ MedicalDocManagment.WebUI/Helpers/PositionMapHelper.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MedicalDocManagment.DAL.Entities;
+using MedicalDocManagment.WebUI.Models;
+using System.Collections.Generic;
+
+namespace MedicalDocManagment.WebUI.Helpers
+{
+    public class PositionMapHelper
+    {
+        public static PositionModel DTOToVM(PositionDTO position)
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<PositionDTO, PositionModel>();
+            });
+            var mapper = config.CreateMapper();
+            return mapper.Map<PositionDTO, PositionModel>(position);
+        }
+        public static List<PositionModel> DTOsToVMs(List<PositionDTO> positions)
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<PositionDTO, PositionModel>();
+            });
+            var mapper = config.CreateMapper();
+            return mapper.Map<IList<PositionDTO>, List<PositionModel>>(positions);
+        }
+    }
+}
